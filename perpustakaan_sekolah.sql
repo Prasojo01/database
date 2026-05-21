19525ee38a2b5636d9ac2d52f24cd12e613b9b0b
Prasojo01 <agprsjo@gmail.com>
2026-05-21 12:33:37 +0700

    Add files via upload

---
+++ perpustakaan_sekolah.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2026 at 07:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan_sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+-- Error reading structure for table perpustakaan_sekolah.buku: #1932 - Table &#039;perpustakaan_sekolah.buku&#039; doesn&#039;t exist in engine
+-- Error reading data for table perpustakaan_sekolah.buku: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `perpustakaan_sekolah`.`buku`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+-- Error reading structure for table perpustakaan_sekolah.peminjaman: #1932 - Table &#039;perpustakaan_sekolah.peminjaman&#039; doesn&#039;t exist in engine
+-- Error reading data for table perpustakaan_sekolah.peminjaman: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `perpustakaan_sekolah`.`peminjaman`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table perpustakaan_sekolah.users: #1932 - Table &#039;perpustakaan_sekolah.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table perpustakaan_sekolah.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `perpustakaan_sekolah`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ toko_online.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2026 at 07:31 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table toko_online.cart: #1932 - Table &#039;toko_online.cart&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`cart`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_messages`
+--
+-- Error reading structure for table toko_online.contact_messages: #1932 - Table &#039;toko_online.contact_messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.contact_messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`contact_messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table toko_online.orders: #1932 - Table &#039;toko_online.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+-- Error reading structure for table toko_online.order_items: #1932 - Table &#039;toko_online.order_items&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`order_items`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table toko_online.products: #1932 - Table &#039;toko_online.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table toko_online.users: #1932 - Table &#039;toko_online.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table toko_online.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `toko_online`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
